90ee193dbb9ef00da6007ab151305014f3a588d5
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-02-05 16:05:44 +0500

    Create Temporary Site Template

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,40 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import logo from "./assets/logo.svg";
+import denimous from "./assets/Denimous.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div className="container">
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            marginTop: "5%",
+          }}
+        >
+          <img
+            style={{ width: "20%", borderRadius: "100%" }}
+            src={logo}
+            alt=""
+          />
+          <img style={{ width: "30%" }} src={denimous} alt="" />
+        </div>
+        <span
+          style={{
+            marginTop: "30px",
+            width: "60%",
+            textAlign: "center",
+            fontSize: "40px",
+            fontWeight: "bold",
+            fontFamily: "sans-serif",
+          }}
+        >
+          The Site is Under Construction
+        </span>
       </div>
-      <h1>I Have Made a Change</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   );
 }
 
